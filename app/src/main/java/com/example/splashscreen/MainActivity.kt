fe060749aa875ea0b6f3ad499ea63d8cd613e2c1
SUMAN1040 <UEMJ2003@GMAIL.COM>
2025-03-13 14:15:49 +0530

    First Change

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -0,0 +1,27 @@
+/*
+ * Project name: Splash Screen
+ * Created by: Suman Kumar Dey
+ * Copyright(c)$ today.year. All rights reserved.
+ * Linkedln: www.linkedin.com/in/suman1040
+ */
+
+package com.example.splashscreen
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/test/java/com/example/splashscreen/ExampleUnitTest.kt
@@ -0,0 +1,24 @@
+/*
+ * Project name: Splash Screen
+ * Created by: Suman Kumar Dey
+ * Copyright(c)$ today.year. All rights reserved.
+ * Linkedln: www.linkedin.com/in/suman1040
+ */
+
+package com.example.splashscreen
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
